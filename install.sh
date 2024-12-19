feb3554a2c27dde6a54f1d5af5fa780214066816
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-12-19 10:01:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/system/bin/env sh
+#
+# Setup pacman in termux
+set -e
 
-mkdir -p tqs
-cd tqs
+cd "${PREFIX%/*}"
+mkdir -p ./tqs
+cd ./tqs
 curl -ZLo tqs.zip 'https://github.com/termux-pacman/termux-packages/releases/latest/download/bootstrap-aarch64.zip'
-unzip tqs.zip && rm -f tqs.zip
-cat SYMLINKS.txt | awk -F "←" '{system("ln -s '"'"'"$1"'"'"' '"'"'"$2"'"'"'")}'
+unzip -v tqs.zip && rm -f tqs.zip
+cat SYMLINKS.txt | awk -F "←" '{system("ln -sv '"'"'"$1"'"'"' '"'"'"$2"'"'"'")}'
 rm -f SYMLINKS.txt
 cd ..
-rm -rf "$PREFIX"
-mv ./tqs "$PREFIX"
+rm -rf "${PREFIX}"
+mv ./tqs "${PREFIX}"
+rm -rf "${HOME}"
